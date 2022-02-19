f60e933e0f5c313975021b70cdc9f230664c6a52
epadams <epaadams2002@gmail.com>
2022-02-19 12:19:59 -0500

    Added JavaDoc to InputHandler

--- InputHandler.java
+++ InputHandler.java
@@ -7,14 +7,20 @@ import java.util.HashMap;
 public class InputHandler {
   private HashMap<String, Command> commands;
 
+  /**
+   * Default constructor that also constructs a hashmap and adds the key value pairs.
+   */
   public InputHandler(Player player) {
     this.commands = new HashMap<String, Command>();
     this.commands.put("jump", JumpCommand(player));
-    this.commands.put("run", RunCommand(player);
-    this.commands.put("fire", FireCommand(player);
+    this.commands.put("run", RunCommand(player));
+    this.commands.put("fire", FireCommand(player));
     this.commands.put("quit", QuitCommand(player));
   }
 
+  /**
+   * Method that deals with user input and executes a command based on it.
+   */
   public void buttonPressed(String button) {
     if (button.equalsIgnoreCase("jump")) {
       this.commands.get("jump").execute();
