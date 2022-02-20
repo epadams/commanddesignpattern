781e69463ee83fe5b0c3899810a0441798236859
Alex Hutton <amhutton@email.sc.edu>
2022-02-20 00:19:49 -0500

    Player Class Skeleton

--- Player.java
+++ Player.java
@@ -1,3 +1,16 @@
 public class Player {
 
+    public Player() {
+        System.out.println("Our Hero is Born");
+    }
+
+    public void jump() {
+    }
+
+    public void fire() {
+    }
+
+    public void runForward() {
+    }
+
 }
