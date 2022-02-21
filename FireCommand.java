41465bb3c1a0dbe313e8d572ef5d00967612909f
willcolumbia <columbiw@email.sc.edu>
2022-02-21 15:28:07 -0500

    done

--- FireCommand.java
+++ FireCommand.java
@@ -1,3 +1,40 @@
-public class FireCommand {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
+public class FireCommand implements Command{
+    public void FireCommand(Player player){
+        
+        File myFile = new File("fire.txt");
+        try {
+            String line = null;
+            BufferedReader br = new BufferedReader(new FileReader(myFile));
+            while((line = br.readLine()) != null){
+                for(int i = 0;i<4;i++){
+                    System.out.println(line);
+                    sleep(3);
+                    clear();
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void execute(){
+        Player.Fire();
+    }
+    private void sleep(int num){
+        try {
+            TimeUnit.MILLISECONDS.sleep(num);
+        } catch (Exception e) {
+            System.out.println("Timmer error");
+        }
+    }
+    
+    private void clear() {
+        System.out.print("\033[H\033[2J");
+    }
 }
--- JumpCommand.java
+++ JumpCommand.java
@@ -1,3 +1,36 @@
-public class JumpCommand {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
+public class JumpCommand implements Command{
+public JumpCommand(Player player) {
+    File myFile = new File("jump.txt");
+    try {
+        String line = null;
+        BufferedReader br = new BufferedReader(new FileReader(myFile));
+        while((line = br.readLine()) != null){
+            for(int i = 0;i<6;i++){
+                System.out.println(line);
+                sleep(3);
+                clear();
+            }
+        }
+
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+private void sleep(int num){
+    try {
+        TimeUnit.MILLISECONDS.sleep(num);
+    } catch (Exception e) {
+        System.out.println("Timmer error");
+    }
+}
+
+private void clear() {
+    System.out.print("\033[H\033[2J");
+}
 }
