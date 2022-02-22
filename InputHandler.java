bc4842a1fbf217cde85398b656f25146d8e72dc5
epadams <epaadams2002@gmail.com>
2022-02-22 13:15:19 -0500

    Updated InputHandler.java

--- InputHandler.java
+++ InputHandler.java
@@ -9,17 +9,21 @@ public class InputHandler {
 
   /**
    * Default constructor that also constructs a hashmap and adds the key value pairs.
+   *
+   * @param player Player to be passed in.
    */
   public InputHandler(Player player) {
     this.commands = new HashMap<String, Command>();
-    this.commands.put("jump", JumpCommand(player));
-    this.commands.put("run", RunCommand(player));
-    this.commands.put("fire", FireCommand(player));
-    this.commands.put("quit", QuitCommand(player));
+    this.commands.put("jump", new JumpCommand(player));
+    this.commands.put("run", new RunCommand(player));
+    this.commands.put("fire", new FireCommand(player));
+    this.commands.put("quit", new QuitCommand(player));
   }
 
   /**
    * Method that deals with user input and executes a command based on it.
+   *
+   * @param button Button that correlates to an action.
    */
   public void buttonPressed(String button) {
     if (button.equalsIgnoreCase("jump")) {
