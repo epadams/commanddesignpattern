462735b614d3f5e90397c25027d6981fb38ff7fd
Alex Hutton <amhutton@email.sc.edu>
2022-02-24 12:22:17 -0500

    Commented Player Class

--- Player.java
+++ Player.java
@@ -1,19 +1,44 @@
+/**
+ * The player class is the hero who can execute the jump, fire and run commands
+ * 
+ * @Author Alex Hutton
+ */
 public class Player {
 
+  /**
+   * The player constructor, states the hero has been born
+   * 
+   * @Author Alex Hutton
+   */
   public Player() {
     System.out.println("Our Hero is Born");
   }
 
+  /**
+   * Jump creates and executes the jump command
+   * 
+   * @Author Alex Hutton
+   */
   public void jump() {
     Command jc = new JumpCommand(this);
     jc.execute();
   }
 
+  /**
+   * Creates and executes the fire command
+   * 
+   * @Author Alex Hutton
+   */
   public void fire() {
     Command f = new FireCommand(this);
     f.execute();
   }
 
+  /**
+   * Creates and executes the run command
+   * 
+   * @Author Alex Hutton
+   */
   public void runForward() {
     Command rf = new RunCommand(this);
     rf.execute();
